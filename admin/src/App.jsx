320d49b638f0052ea1ee1e6e855de946c75bf151
Rahul Kumar <rahulptl2022@gmail.com>
2025-04-08 01:09:51 +0530

    completed Admin login

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import { AdminContext } from './context/AdminContext';
 
 const App = () => {
-  return (
+  
+  const {aToken} = useContext(AdminContext)
+  
+  return aToken ? (
     <div>
-      
+      <ToastContainer />
     </div>
+  ) : (
+      <div>
+        <Login />
+        <ToastContainer />
+      </div>
   )
 }
 
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,11 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import  AdminContextProvider  from "./context/AdminContext";
+import  AppContextProvider  from "./context/AppContext";
+import  DoctorContextProvider  from "./context/DoctorContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <AdminContextProvider>
+      <DoctorContextProvider>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </DoctorContextProvider>
+    </AdminContextProvider>
+  </BrowserRouter>
+);
